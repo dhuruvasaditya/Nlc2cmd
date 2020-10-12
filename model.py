8a721e8caf04640e61d170f1b47696c5dd351020
reshinthadithyan <reshinther@gmail.com>
2020-10-12 19:50:41 +0530

    model.py

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 from torch.nn import Module
+from torch import Tensor
+
+class OmniBash(Module):
+    def __init__(self,Transformer,Device):
+        self.Trans = Transformer # Transformer Model
+        self.Device = Device
+    def forward(self,Input,Label):
+        Attn_Mask = Labels
+        Output = self.Trans(Input,attention_mask=Attn_Mask)
